a6c9cea555f32f5949a8a4d8b3d66b663a25eec6
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-17 06:42:22 +0300

    Added a Throw Transition

--- Assets/HC Cricket/Scripts/PlayerBowler.cs
+++ Assets/HC Cricket/Scripts/PlayerBowler.cs
@@ -4,28 +4,75 @@ using UnityEngine;
 
 public class PlayerBowler : MonoBehaviour
 {
+    public enum State { Idle, Aiming, Running, Bowling }
+
     [Header(" Elements ")]
     [SerializeField] private Animator animator;
 
-
     [Header(" Settings ")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float runDuration;
+    private float runTimer;
 
-    
+    private State state;
+
+
+
     private void Start()
     {
-        animator.Play("Run");
+        StartRunning();
     }
 
-    
+
     private void Update()
     {
-        Run();
+        ManageState();
+    }
+
+
+    private void ManageState()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break;
+
+            case State.Aiming:
+                break;
+
+            case State.Running:
+                Run();
+                break;
+
+            case State.Bowling:
+                break;
+        }
+    }
+
+
+    private void StartRunning()
+    {
+        state = State.Running;
+        animator.SetInteger("State", 1);
     }
 
 
     private void Run()
     {
         transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+
+        runTimer += Time.deltaTime;
+
+        if (runTimer >= runDuration)
+        {
+            StartBowling();
+        }
+    }
+
+
+    private void StartBowling()
+    {
+        state = State.Bowling;
+        animator.SetInteger("State", 2);
     }
 }
