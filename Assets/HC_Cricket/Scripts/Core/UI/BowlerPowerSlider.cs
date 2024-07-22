9586648e50593c6f752022a766af7166c09d05a4
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-22 11:20:49 +0300

    Added slider script

---
+++ Assets/HC_Cricket/Scripts/Core/UI/BowlerPowerSlider.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BowlerPowerSlider : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private Slider powerSlider;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
