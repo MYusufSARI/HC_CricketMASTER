c9e5576d6d04875fae194f023db96487609a8fc5
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-23 16:26:02 +0300

    Added animation logic

--- Assets/HC_Cricket/Scripts/Core/Batsman/AIBatsman.cs
+++ Assets/HC_Cricket/Scripts/Core/Batsman/AIBatsman.cs
@@ -6,10 +6,16 @@ public class AIBatsman : MonoBehaviour
 {
     [Header(" Elements ")]
     [SerializeField] private BowlerTarget bowlerTarget;
+    [SerializeField] private Animator _animator;
 
 
     [Header(" Settings ")]
     [SerializeField] private float moveSpeed;
+
+
+    private const string IDLE = "Idle";
+    private const string LEFT_WALKING = "Left";
+    private const string RIGHT_WALKING = "Right";
 
 
 
@@ -23,6 +29,27 @@ public class AIBatsman : MonoBehaviour
     {
         Vector3 targetPosition = transform.position;
         targetPosition.x = bowlerTarget.transform.position.x;
+
+        // Calculate how far we are from the target
+        float difference = targetPosition.x - transform.position.x;
+
+        if (difference ==0)
+        {
+            // Play the Idle animation
+            _animator.Play(IDLE);
+        }
+
+        else if (difference >0)
+        {
+            // Play the left strafe animation
+            _animator.Play(LEFT_WALKING);
+        }
+
+        else
+        {
+            // Play the right strafe animation
+            _animator.Play(RIGHT_WALKING);
+        }
 
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
