6f1a4307c63e693aee6cf74bfe03d47c5f29adc2
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-23 16:21:17 +0300

    Added AI

---
+++ Assets/HC_Cricket/Scripts/Core/Batsman/AIBatsman.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBatsman : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private BowlerTarget bowlerTarget;
+
+
+    [Header(" Settings ")]
+    [SerializeField] private float moveSpeed;
+
+
+
+    private void Update()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        Vector3 targetPosition = transform.position;
+        targetPosition.x = bowlerTarget.transform.position.x;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
