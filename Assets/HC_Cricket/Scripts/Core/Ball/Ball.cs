a80b034068134da3e09da84d3e4bb5c4eb84e780
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-24 04:14:10 +0300

    Added score of the ball logic

---
+++ Assets/HC_Cricket/Scripts/Core/Ball/Ball.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [Header(" Settings ")]
+    private bool hasHitGround;
+
+
+    [Header(" Events ")]
+    public static Action<Vector3> onBallHitGround;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Detect if the ball touches the field
+        if (collision.collider.tag == "Field")
+        {
+            FieldCollidedCallback();
+        }
+    }
+
+
+    private void FieldCollidedCallback()
+    {
+        if (hasHitGround)
+        {
+            return;
+        }
+
+        hasHitGround = true;
+
+        onBallHitGround?.Invoke(transform.position);
+    }
+}
