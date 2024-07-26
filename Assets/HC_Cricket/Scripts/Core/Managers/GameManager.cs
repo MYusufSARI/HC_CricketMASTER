6036ecdc9a67995790f9b2240c36c473ce5c01f8
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-26 15:20:08 +0300

    .

--- Assets/HC_Cricket/Scripts/Core/Managers/GameManager.cs
+++ Assets/HC_Cricket/Scripts/Core/Managers/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour
 
         onGameSet?.Invoke();
 
-        StartGame();
+        Invoke("StartGame", 2);
     }
 
 
---
+++ Assets/HC_Cricket/Scripts/Core/UI/MenuManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private CanvasGroup transitionCG;
+    [SerializeField] private TextMeshProUGUI transitionGameModeText;
+
+
+    private void Start()
+    {
+        GameManager.onGameSet += GameSetCallback;
+    }
+
+
+    private void OnDestroy()
+    {
+        GameManager.onGameSet -= GameSetCallback;
+    }
+
+
+    private void GameSetCallback()
+    {
+        if (GameManager.instance.IsBowler())
+        {
+            transitionGameModeText.text = "BOWL";
+        }
+
+        else
+        {
+            transitionGameModeText.text = "BAT";
+        }
+
+        // Show the TransitionPanel
+        transitionCG.alpha = 1;
+        transitionCG.blocksRaycasts = true;
+        transitionCG.interactable = true;
+    }
+}
